0b6f5db7ec73ae4541b145746ced89d77af4b463
Junghwan John Goh <jhgoh@cern.ch>
2015-06-04 16:40:19 +0200

    moving to test area, add nhit

--- DQMAnalysis/test/extractRPCBlaclist.py
+++ DQMAnalysis/test/extractRPCBlaclist.py
@@ -6,13 +6,17 @@ from ROOT import *
 minEvents = 10000
 minDeadFrac = 0.8
 
-basePath = "/store1/chanwook/data"
+#basePath = "/store1/chanwook/data"
+basePath = "/store1/jhgoh/DQM/SingleMu/Commissioning2015-PromptReco-v1/"
 #basePath = "/store1/jhgoh/RPC/DQM/SingleMu2012"
 #basePath = "/store1/jhgoh/RPC/DQM/Cosmics_2015"
 
 nRoll = 0
 nRun = 0
 rollsByRun = {}
+
+hRateBarrel = TH1F("hRateBarrel", "Barrel;Strip occupancy per event", 1001, -1e-5, 1e-2)
+hRateEndcap = TH1F("hRateEndcap", "Endcap;Strip occupancy per event", 1001, -1e-5, 1e-2)
 
 for fName in sorted(os.listdir(basePath)):
     if not fName.startswith("DQM"): continue
@@ -83,12 +87,20 @@ for fName in sorted(os.listdir(basePath)):
                         effYbin = hEff.GetYaxis().FindBin("%s_%s" % (prefix, suffix))
                         eff = hEff.GetBinContent(effXbin, effYbin)
 
-                        ndead = 0.
+                        ndead, nhit = 0., 0
                         for istrip in range(nPerRoll):
                             bin = 1+istrip+nPerRoll*iroll
-                            if h.GetBinContent(bin) == 0: ndead += 1
+                            occup = h.GetBinContent(bin)
+                            nhit += occup
+                            if occup == 0:
+                                ndead += 1
+                                hRateBarrel.Fill(-1e-6)
+                            else:
+                                rate = occup/nRPCEvents
+                                hRateBarrel.Fill(rate)
+
                         fdead = ndead/nPerRoll
-                        rollToFdead[rollName] = (fdead, eff)
+                        rollToFdead[rollName] = (fdead, nhit, eff)
 
     for disk in range(-4, 5):
         if disk == 0: continue
@@ -117,41 +129,63 @@ for fName in sorted(os.listdir(basePath)):
                         effXbin, effYbin = segment, 1+iroll+3*(ring-2)
                         eff = hEff.GetBinContent(effXbin, effYbin)
 
-                        ndead = 0.
+                        ndead, nhit  = 0., 0
                         for istrip in range(nPerRoll):
                             bin = 1+istrip+nPerRoll*iroll
-                            if h.GetBinContent(bin) == 0: ndead += 1
+                            occup = h.GetBinContent(bin)
+                            nhit += occup
+                            if occup == 0:
+                                ndead += 1
+                                hRateEndcap.Fill(-1e-6)
+                            else:
+                                rate = occup/nRPCEvents
+                                hRateEndcap.Fill(rate)
+
                         fdead = ndead/nPerRoll
-                        rollToFdead[rollName] = (fdead, eff)
+                        rollToFdead[rollName] = (fdead, nhit, eff)
 
     nRun += 1
     nRoll = max(nRoll, len(rollToFdead))
-    rollsByRun[runNumber] = (rollToFdead, nLSRPC)
+    rollsByRun[runNumber] = (rollToFdead, nLSRPC, nRPCEvents)
 
-hFdeadVsEff = TH2F("fdeadVsEff", "fdead vs efficiency;1-(Dead fraction) [%%];Efficiency [%%]", 102, -1, 101, 102, -1, 101)
-hRdeadVsEff = TH2F("rdeadVsEff", "fdead/LS vs efficiency;(1-(Dead fraction))/nLS [%%];Efficiency [%%]", 102, -1, 101, 102, -1, 101)
+hFdeadVsEff = TH2F("fdeadVsEff", "fdead vs efficiency;1-(Dead fraction) [%];Efficiency [%]", 102, -1, 101, 103, -2, 101)
 hFdeadHistory = TH2F("fdeadHistory", "fdeadHistory", nRun, 0, nRun, nRoll, 0, nRoll)
-hRdeadHistory = TH2F("rdeadHistory", "rdeadHistory", nRun, 0, nRun, nRoll, 0, nRoll)
+hNhitVsEff = TH2F("hNhitVsEff", "nhit vs efficiency;RPC hit multiplicity;Efficiency [%]", 50, 0, 1000, 103, -2, 101)
 for irun, run in enumerate(sorted(rollsByRun.keys())):
-    rollToFdead, nLS = rollsByRun[run]
+    rollToFdead, nLS, nRPCEvents = rollsByRun[run]
 
-    fout = open("fDead_run%d.txt" % run, "w")
-    print>>fout, "# Run = %d" % run
-    print>>fout, "# nLS with RPC on = %d" % nLSRPC
-    print>>fout, "# Roll_Name\tnDeadStrip/nStrip"
+    outFdead = open("fDead_run%d.txt" % run, "w")
+    outBlist = open("Blacklist_run%d.txt" % run, "w")
+    print>>outFdead, "# Run = %d" % run
+    print>>outFdead, "# nLS with RPC on = %d" % nLS
+    print>>outFdead, "# nEvents with RPC hit = %d" % nRPCEvents
+    print>>outFdead, "# Roll_Name\tnDeadStrip/nStrip\tEfficiency"
+
+    print>>outBlist, "# Run = %d" % run
+    print>>outBlist, "# nLS with RPC on = %d" % nLS
+    print>>outBlist, "# nEvents with RPC hit = %d" % nRPCEvents
+    print>>outBlist, "# Roll_Name\tnDeadStrip/nStrip\tEfficiency"
 
     for iroll, roll in enumerate(sorted(rollToFdead.keys())):
-        (fdead, eff) = rollToFdead[roll]
+        (fdead, nhit, eff) = rollToFdead[roll]
         if eff == 1.0: eff += 1e-9
+        if eff <= -0.9: eff = -2
         if eff == 0.0: eff -= 1e-9
-        print>>fout, "%s\t%f" % (roll, fdead)
+        print>>outFdead, "%s\t%f\t%f\t%f" % (roll, fdead, nhit, eff)
+        if fdead >= 0.8: print>>outBlist, "%s\t%f\t%f\t%f" % (roll, fdead, nhit, eff) 
 
-        hFdeadHistory.SetBinContent(irun+1, iroll+1, 1-fdead)
-        hRdeadHistory.SetBinContent(irun+1, iroll+1, (1-fdead)/nLS)
+        hFdeadHistory.SetBinContent(irun+1, iroll+1, fdead)
         hFdeadVsEff.Fill((1-fdead)*100, eff)
-        hRdeadVsEff.Fill((1-fdead)*100/nLS, eff)
+        hNhitVsEff.Fill(min(999, nhit), eff)
 
-    fout.close()
+    outFdead.close()
+    outBlist.close()
+
+cRate = TCanvas("cRate", "cRate", 500, 500)
+hRateBarrel.SetLineColor(kRed)
+hRateEndcap.SetLineColor(kBlue)
+hRateBarrel.Draw()
+hRateEndcap.Draw("sames")
 
 cFHistory = TCanvas("cFHistory", "cFHistory", 500, 500)
 hFdeadHistory.Draw("COLZ")
@@ -159,8 +193,5 @@ hFdeadHistory.Draw("COLZ")
 cFCorr = TCanvas("cFCorr", "cFCorr", 500, 500)
 hFdeadVsEff.Draw("COLZ")
 
-cRHistory = TCanvas("cRHistory", "cRHistory", 500, 500)
-hRdeadHistory.Draw("COLZ")
-
-cRCorr = TCanvas("cRCorr", "cRCorr", 500, 500)
-hRdeadVsEff.Draw("COLZ")
+cNCorr = TCanvas("cNCorr", "cNCorr", 500, 500)
+hNhitVsEff.Draw("COLZ")
