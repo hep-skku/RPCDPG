3b4468a89ff0e61e753df2286220bb21aec3b4a8
Junghwan John Goh <jhgoh@cern.ch>
2015-10-20 12:49:36 +0200

    examples for Run2015B and D

--- RecHitAnalysis/test/crabConfig.py
+++ RecHitAnalysis/test/crabConfig.py
@@ -2,18 +2,22 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
 config.General.requestName = 'RPCAnalysis'
-config.General.workArea = 'crab_ZeroBias_Run2015C'
+config.General.workArea = 'crab_ZeroBias_Run2015B'
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'trackToRPCNtuple_cfg.py'
 
-config.Data.inputDataset = '/ZeroBias/Run2015C-PromptReco-v1/RECO'
+config.Data.inputDataset = '/ZeroBias/Run2015B-PromptReco-v1/RECO'
+#config.Data.inputDataset = '/ZeroBias/Run2015C-PromptReco-v1/RECO'
+#config.Data.inputDataset = '/ZeroBias/Run2015D-PromptReco-v3/RECO'
+#config.Data.inputDataset = '/ZeroBias/Run2015D-PromptReco-v4/RECO'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 50
-config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-258714_13TeV_PromptReco_Collisions15_25ns_JSON_MuonPhys.txt'
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-255031_13TeV_PromptReco_Collisions15_50ns_JSON_MuonPhys_v2.txt'
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-258714_13TeV_PromptReco_Collisions15_25ns_JSON_MuonPhys.txt'
 config.Data.runRange = '246908-258714'
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = False
