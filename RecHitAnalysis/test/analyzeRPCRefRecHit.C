2170ae3dd654fc525061274c9f8b71b8aed01307
chanwook <hcwhwang@gmail.com>
2015-10-22 12:30:53 +0200

    without eff.

--- RecHitAnalysis/test/analyzeRPCRefRecHit.C
+++ RecHitAnalysis/test/analyzeRPCRefRecHit.C
@@ -26,7 +26,7 @@ void analyzeRPCRefRecHit(TString fileName, TString outFileName = "result.root") 
     TH1F* hRegionAll = new TH1F("hRegionAll", "All", 4, -2, 2);
     TH1F* hRegionPass = new TH1F("hRegionPass", "Pass", 4, -2, 2);
     hRegionAll->GetXaxis()->SetBinLabel(1, "All");
-    hRegionPass->GetXaxis()->SetBinLabel(1, "All");
+    hRegionPass->GetXaxis()->SetBinLabel(1, "Pass");
     hAll[Key()] = hRegionAll;
     hPass[Key()] = hRegionPass;
     for ( int region=-1; region<=1; ++region ) {
@@ -45,6 +45,7 @@ void analyzeRPCRefRecHit(TString fileName, TString outFileName = "result.root") 
       if ( region == 0 ) {
         TH1F* hWheelAll  = new TH1F("hWheelAll", "All;Wheel", 5, -2.5, 2.5);
         TH1F* hWheelPass = new TH1F("hWheelPass", "Pass;Wheel", 5, -2.5, 2.5);
+
         hAll[Key({region})] = hWheelAll;
         hPass[Key({region})] = hWheelPass;
 
@@ -53,9 +54,46 @@ void analyzeRPCRefRecHit(TString fileName, TString outFileName = "result.root") 
           TDirectory* wheelDir = regionDir->GetDirectory(Form("Wheel%+d", wheel));
           wheelDir->cd();
 
+          TH1F* hStationAll = new TH1F("hStationAll", "All;Station",4, 0.5, 4.5);
+          TH1F* hStationPass = new TH1F("hStationPass", "Pass;Station",4, 0.5, 4.5);
+
+          hAll[Key({region,wheel})] = hStationAll;
+          hPass[Key({region,wheel})] = hStationPass;
+
+
           for ( int station=1; station<=4; ++station ) {
+            wheelDir->mkdir(Form("Station%+d",station));
+            TDirectory* stationDir = wheelDir->GetDirectory(Form("Station%+d",station));
+            stationDir->cd();
+
+            TH1F* hSectorAll = new TH1F("hSectorAll","All;Sector", 12, 0.5, 12.5);
+            TH1F* hSectorPass = new TH1F("hSectorPass","Pass;Sector", 12, 0.5, 12.5);
+
+            hAll[Key({region,wheel,station})] = hSectorAll;
+            hPass[Key({region,wheel,station})] = hSectorPass;
+
             for ( int sector=1; sector<=12; ++sector ) {
+              stationDir->mkdir(Form("Sector%+d",sector));
+              TDirectory* sectorDir = stationDir->GetDirectory(Form("Sector%+d",sector));
+              sectorDir->cd();
+
+              TH1F* hLayerAll = new TH1F("hLayerAll","All;Layer", 2, 0.5, 2.5);
+              TH1F* hLayerPass = new TH1F("hLayerPass","Pass;Layer", 2, 0.5, 2.5);
+
+              hAll[Key({region,wheel,station,sector})] = hLayerAll;
+              hPass[Key({region,wheel,station,sector})] = hLayerPass;
+
               for ( int layer=1; layer<=2; ++layer ) {
+                sectorDir->mkdir(Form("Layer%+d",layer));
+                TDirectory* layerDir = sectorDir->GetDirectory(Form("Layer%+d",layer));
+                layerDir->cd();
+
+                TH1F* hRollAll = new TH1F("hRollAll","All;Roll", 3, 0.5, 3.5);
+                TH1F* hRollPass = new TH1F("hRollPass","Pass;Roll", 3, 0.5, 3.5);
+
+                hAll[Key({region,wheel,station,sector,layer})] = hRollAll;
+                hPass[Key({region,wheel,station,sector,layer})] = hRollPass;
+
               }
             }
           }
@@ -75,10 +113,45 @@ void analyzeRPCRefRecHit(TString fileName, TString outFileName = "result.root") 
           TDirectory* diskDir = regionDir->GetDirectory(Form("Disk%+d", region*disk));
           diskDir->cd();
 
+          TH1F* hSectorAll = new TH1F("hSectorAll", "All;Sector", 12, 0.5, 12.5);
+          TH1F* hSectorPass = new TH1F("hSectorPass", "Pass;Sector", 12, 0.5, 12.5);
+
+          hAll[Key({region,disk})] = hSectorAll;
+          hPass[Key({region,disk})] = hSectorPass;
+
           for ( int sector=1; sector<=12; ++sector ) {
+            diskDir->mkdir(Form("Sector%+d",sector));
+            TDirectory* sectorDir = diskDir->GetDirectory(Form("Sector%+d",sector));
+            sectorDir->cd();
+
+            TH1F* hStationAll = new TH1F("hStationAll","All;Station", 4, 0.5, 4.5);
+            TH1F* hStationPass = new TH1F("hStationPass","Pass;Station", 4, 0.5, 4.5);
+
+            hAll[Key({region,disk,sector})] = hStationAll;
+            hPass[Key({region,disk,sector})] = hStationPass;
             for ( int station=1; station<=4; ++station ) {
               // For endcap - "C++ sector" is "Ring" (how far from beamline)
+              sectorDir->mkdir(Form("Station%+d",station));
+              TDirectory* stationDir = sectorDir->GetDirectory(Form("Station%+d",station));
+              stationDir->cd();
+
+              TH1F* hLayerAll = new TH1F("hLayerAll","All;Layer", 2, 0.5, 2.5);
+              TH1F* hLayerPass = new TH1F("hLayerPass","Pass;Layer", 2, 0.5, 2.5);
+
+              hAll[Key({region,disk,sector,station})] = hLayerAll;
+              hPass[Key({region,disk,sector,station})] = hLayerPass;
+
               for ( int layer=1; layer<=2; ++layer ) {
+                stationDir->mkdir(Form("Layer%+d",layer));
+                TDirectory* layerDir = stationDir->GetDirectory(Form("Layer%+d",layer));
+                layerDir->cd();
+
+                TH1F* hRollAll = new TH1F("hRollAll","All;Roll", 3, 0.5, 3.5);
+                TH1F* hRollPass = new TH1F("hRollPass","Pass;Roll", 3, 0.5, 3.5);
+
+                hAll[Key({region,disk,sector,station,layer})] = hRollAll;
+                hPass[Key({region,disk,sector,station,layer})] = hRollPass;
+
               }
             }
           }
@@ -119,22 +192,68 @@ void analyzeRPCRefRecHit(TString fileName, TString outFileName = "result.root") 
       hAll[Key({recHit.region})]->Fill(recHit.ring);
       if ( recHit.region == 0 ) {
         // For barrel - "C++ ring" is "Wheel"
-        //hAll[Key({recHit.region, recHit.ring})]->Fill(recHit.station);
-        //hAll[Key({recHit.region, recHit.ring, recHit.station})]->Fill(recHit.sector);
+        hAll[Key({recHit.region, recHit.ring})]->Fill(recHit.station);
+        hAll[Key({recHit.region, recHit.ring, recHit.station})]->Fill(recHit.sector);
+        hAll[Key({recHit.region, recHit.ring, recHit.station, recHit.sector})]->Fill(recHit.layer);
+        hAll[Key({recHit.region, recHit.ring, recHit.station, recHit.sector, recHit.layer})]->Fill(recHit.roll);
       }
       else {
         // For endcap - "C++ ring" is "Disk" (how apart from the barrel)
         // For endcap - "C++ sector" is "Ring" (how far from beamline)
-        //hAll[Key({recHit.region, recHit.ring})]->Fill(recHit.station);
+        hAll[Key({recHit.region, recHit.ring})]->Fill(recHit.sector);
+        hAll[Key({recHit.region, recHit.ring, recHit.sector})]->Fill(recHit.station);
+        hAll[Key({recHit.region, recHit.ring, recHit.sector, recHit.station})]->Fill(recHit.layer);
+        hAll[Key({recHit.region, recHit.ring, recHit.sector, recHit.station, recHit.layer})]->Fill(recHit.roll);
       }
 
       if ( recHit.dx != -999 ) {
         hPass[Key()]->Fill(recHit.region);
         hPass[Key({recHit.region})]->Fill(recHit.ring);
+        if ( recHit.region == 0 ) {
+          hPass[Key({recHit.region, recHit.ring})]->Fill(recHit.station);
+          hPass[Key({recHit.region, recHit.ring, recHit.station})]->Fill(recHit.sector);
+          hPass[Key({recHit.region, recHit.ring, recHit.station, recHit.sector})]->Fill(recHit.layer);
+          hPass[Key({recHit.region, recHit.ring, recHit.station, recHit.sector, recHit.layer})]->Fill(recHit.roll);
+        }
+        else {
+          hPass[Key({recHit.region, recHit.ring})]->Fill(recHit.sector);
+          hPass[Key({recHit.region, recHit.ring, recHit.sector})]->Fill(recHit.station);
+          hPass[Key({recHit.region, recHit.ring, recHit.sector, recHit.station})]->Fill(recHit.layer);
+          hPass[Key({recHit.region, recHit.ring, recHit.sector, recHit.station, recHit.layer})]->Fill(recHit.roll);
+        } 
       }
     }
   }
-
+  //std::map<Key, TEfficiency*> hEff;
+  //hEff[Key()]=
+  TEfficiency* hEff = 0;
+  outFile->cd();
+  //hEff = new TEfficiency(hPass[Key()],hAll[Key()]);
+  hEff = new TEfficiency(hRollPass,hRollAll);
+  hEff->Write();
+  //hEff->SetDirectory(outFile);
+  for ( int region=-1; region<=1; ++region) {
+    if ( region == 0) {
+      for ( int ring=-2; ring<=2; ++ring) {
+        for ( int station=1; station<=4; ++station) {
+          for ( int sector=1; sector<=12; ++sector) {
+            for ( int layer=1; layer<=2; ++layer) {
+            }
+          }
+        }
+      }
+    }
+    else {
+      for ( int ring=1; ring<=4; ++ring) {
+        for ( int sector=1; sector<=12; ++sector) {
+          for ( int station=1; station<=4; ++station) {
+            for ( int layer=1; layer<=2; ++layer) {
+            }
+          }
+        }
+      }
+    }
+  }
   hAll[Key()]->SetBinContent(1, hAll[Key()]->Integral());
   hPass[Key()]->SetBinContent(1, hPass[Key()]->Integral());
   outFile->Write();
