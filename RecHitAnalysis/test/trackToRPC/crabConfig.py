1aa4674ec5ca17fac45bb3434ab4eb53f86a4fc0
Junghwan John Goh <jhgoh@cern.ch>
2015-11-17 03:04:56 +0100

    improve crabConfig

--- RecHitAnalysis/test/trackToRPC/crabConfig.py
+++ RecHitAnalysis/test/trackToRPC/crabConfig.py
@@ -1,29 +1,30 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'RPCAnalysis'
+config.General.requestName = 'TrackToRPC'
 #config.General.workArea = 'crab_ZeroBias_Run2015D_v3'
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'trackToRPCNtuple_cfg.py'
+config.JobType.psetName = 'run_cfg.py'
 
-config.Data.inputDataset = '/ZeroBias/Run2015B-PromptReco-v1/RECO'
+#config.Data.inputDataset = '/ZeroBias/Run2015B-PromptReco-v1/RECO'
 #config.Data.inputDataset = '/ZeroBias/Run2015C-PromptReco-v1/RECO'
 #config.Data.inputDataset = '/ZeroBias/Run2015D-PromptReco-v3/RECO'
-#config.Data.inputDataset = '/ZeroBias/Run2015D-PromptReco-v4/RECO'
+config.Data.inputDataset = '/ZeroBias/Run2015D-PromptReco-v4/RECO'
 
 config.General.workArea = 'crab_'+config.Data.inputDataset.split('/')[2].replace('-', '_')
 
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 50
+lumiMaskBase = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/'
 if 'Run2015B' in config.Data.inputDataset:
-    config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-255031_13TeV_PromptReco_Collisions15_50ns_JSON_MuonPhys_v2.txt'
+    config.Data.lumiMask = lumiMaskBase+'Cert_246908-255031_13TeV_PromptReco_Collisions15_50ns_JSON_MuonPhys_v2.txt'
 elif 'Run2015' in config.Data.inputDataset:
-    config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-258714_13TeV_PromptReco_Collisions15_25ns_JSON_MuonPhys.txt'
-config.Data.runRange = '246908-258714'
+    config.Data.lumiMask = lumiMaskBase+'Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_MuonPhys.txt'
+config.Data.runRange = '246908-260627'
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = False
 #config.Data.publishDataName = 'CRAB3_tutorial_May2015_Data_analysis'
